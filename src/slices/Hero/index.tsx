bec194484d945754472d6b6f736a49fafe7110f4
markthos <aydenthos@gmail.com>
2024-02-20 11:56:17 -0800

    added some styling and layout

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -15,9 +15,19 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      <span className="text-red-500">{slice.primary.first_name}</span>
-      <>{slice.primary.last_name}</>
-      <>{slice.primary.tag_line}</>
+      <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
+        <div className="col-start-1 md:col-start-1">
+          <h1 className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter" 
+              aria-label={
+                slice.primary.first_name + " " + slice.primary.last_name
+                }
+          >
+            <span className="blocvk text-slate-300">{slice.primary.first_name}</span>
+            <span className="-mt-[.2em] block text-slate-500">{slice.primary.last_name}</span>
+          </h1>
+            <span className="block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-100 md:text-4xl">{slice.primary.tag_line}</span>
+        </div>
+      </div>
     </section>
   );
 };
