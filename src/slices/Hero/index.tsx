8f2ea6be8b3492c627adaada6a85fe3f15d226eb
markthos <aydenthos@gmail.com>
2024-02-20 13:33:16 -0800

    added bounded props for recentering the site

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -5,6 +5,7 @@ import { useEffect, useRef } from "react";
 import { Content, KeyTextField } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
 import gsap from "gsap";
+import Bounded from "@/components/bounded";
 
 /**
  * Props for `Hero`.
@@ -74,7 +75,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
 
 
     return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
       ref={component}
@@ -96,7 +97,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
             <span className="job-title block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-0 md:text-4xl">{slice.primary.tag_line}</span>
         </div>
       </div>
-    </section>
+    </Bounded>
   );
 };
 
