868513cb7f78c1819f6b4713a89a18aef1b41c6f
markthos <aydenthos@gmail.com>
2024-02-20 13:17:42 -0800

    added gsap animations

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,5 +1,10 @@
-import { Content } from "@prismicio/client";
+'use client'
+
+import { useEffect, useRef } from "react";
+
+import { Content, KeyTextField } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
+import gsap from "gsap";
 
 /**
  * Props for `Hero`.
@@ -10,10 +15,69 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  * Component for "Hero" Slices.
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
-  return (
+  const component = useRef(null)
+
+    useEffect(() => {
+      let ctx = gsap.context(() => {
+        const tl = gsap.timeline();
+
+        tl.fromTo(".name-animation", {
+            x: -100,
+            opacity: 0,
+            rotate: -10,
+          }, 
+          {
+            x: 0,
+            opacity: 1,
+            rotate: 0,
+            stagger: {
+              each: 0.05,
+              from: "random",
+            },
+            duration: 1.5,
+            ease: "elastic.out(1,0.3)",
+            transformOrigin: "left top",
+            delay: 0.5,
+          }
+        );
+
+        tl.fromTo(".job-title", {
+            y: 20,
+            opacity: 0,
+          }, 
+          {
+            y: 0,
+            opacity: 1,
+            duration: 1.5,
+            scale: 1,
+            ease: "elastic.out(1,0.3)",
+          }
+        );
+      }, 
+      component);
+
+      return () => ctx.revert();
+    }, []);
+
+  const renderLettters = (name:KeyTextField, key:string) => {
+    if (!name) return;
+    return name.split("").map((letter, index) => (
+      <span 
+        key={index} 
+        className={`name-animation name-animation-${key} inline-block opacity-0`}
+      >
+        {letter}
+        </span>
+    ));
+  };
+
+
+
+    return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      ref={component}
     >
       <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
         <div className="col-start-1 md:col-start-1">
@@ -22,10 +86,14 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
                 slice.primary.first_name + " " + slice.primary.last_name
                 }
           >
-            <span className="blocvk text-slate-300">{slice.primary.first_name}</span>
-            <span className="-mt-[.2em] block text-slate-500">{slice.primary.last_name}</span>
+            <span className="blocvk text-slate-300">
+              {renderLettters(slice.primary.first_name, "first")}
+            </span>
+            <span className="-mt-[.2em] block text-slate-500">
+              {renderLettters(slice.primary.last_name, "last")}
+            </span>
           </h1>
-            <span className="block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-100 md:text-4xl">{slice.primary.tag_line}</span>
+            <span className="job-title block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-0 md:text-4xl">{slice.primary.tag_line}</span>
         </div>
       </div>
     </section>
