0d5bf9b034695f9f8c9f46e36c2977b75a552378
markthos <aydenthos@gmail.com>
2024-02-20 18:30:10 -0800

    small issues fixed

---
+++ src/components/header.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { createClient } from '@/prismicio';
+import Link from 'next/link';
+
+
+export default async function Header() {
+    const client = createClient();
+    const settings = await client.getSingle('settings');
+    return (
+        <header className="top-0 z-50 mx-auto max-w-7x1 md:sticky md:top-4">
+            <nav>
+                <ul />
+                <li> 
+                    <Link href="/" aria-label="Home Page">
+                        {settings.data.name}
+                    </Link>
+                </li>
+            </nav>
+        </header>
+    );
+}
